81a29fc541c71d7b88869785cae458692152dabd
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2023-10-02 00:28:37 -0400

    Reformatted Motorcycle.java and PickupTruck.java

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -2,6 +2,7 @@ class Motorcycle extends Vehicle {
     private static final int NUM_OF_WHEELS = 2;
     private static final int PERSON_CAPACITY = 2;
     private static final double MILES_PER_GALLON = 75;
+
     Motorcycle(String name, Color color) {
         super(name + " the Motorcycle", color, NUM_OF_WHEELS, PERSON_CAPACITY, MILES_PER_GALLON);
     }
--- src/PickupTruck.java
+++ src/PickupTruck.java
@@ -2,6 +2,7 @@ class PickupTruck extends Vehicle {
     private static final int NUM_OF_WHEELS = 4;
     private static final int PERSON_CAPACITY = 2;
     private static final double MILES_PER_GALLON = 15;
+
     PickupTruck(String name, Color color) {
         super(name + " the Pickup Truck", color, NUM_OF_WHEELS, PERSON_CAPACITY, MILES_PER_GALLON);
     }
